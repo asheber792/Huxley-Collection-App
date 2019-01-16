d0f0937b3bd33fb6197b4b64aa25ee25160e8a8a
ASHEBER ARLAIN <ASHEBERARLAIN@GMAIL.COM>
2019-01-15 21:59:55 -0500

    installed react router, added barebones jsx to app

--- p2/huxley-collection-app/src/App.js
+++ p2/huxley-collection-app/src/App.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Route, Link } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <div>
+        <header>
+          <nav>
+            <ul>
+              <li><h1>HOME</h1></li>
+              <li><h1>COLLECTED WORKS</h1></li>
+            </ul>
+          </nav>
+        </header>
+        <main>
+        </main>
       </div>
     );
   }
