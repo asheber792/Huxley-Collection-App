bcab5a4cf019b16dd875dab360870f51f6a01bd0
ASHEBER ARLAIN <ASHEBERARLAIN@GMAIL.COM>
2019-01-17 01:10:00 -0500

    added redirect from root path to home

--- p2/huxley-collection-app/src/components/App/App.js
+++ p2/huxley-collection-app/src/components/App/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Route } from "react-router-dom";
+import { Route, Redirect } from "react-router-dom";
 import Header from '../Header/Header'
 import Quotes from '../Quotes/Quotes'
 import Works from '../Works/Works'
@@ -11,6 +11,7 @@ class App extends Component {
       <div>
         <Header />
         <main>
+        	<Redirect from="/" to="/home" />
         	<Route path="/home" component={Quotes} />
         	<Route path="/collected-works" component={Works} />
         </main>
